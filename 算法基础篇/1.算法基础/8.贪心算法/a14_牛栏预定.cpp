85949e47cd7b61a819f3cb20588222786b508718
Mercury1000 <3041492091@qq.com>
2026-03-31 15:48:16 +0800

    算法基础

--- 算法基础篇/1.算法基础/8.贪心算法/a14_牛栏预定.cpp
+++ 算法基础篇/1.算法基础/8.贪心算法/a14_牛栏预定.cpp
@@ -1,56 +1,62 @@
 #include <iostream>
-#include <map>
+#include <queue>
 #include <algorithm>
+
 using namespace std;
+
 const int N = 5e4 + 10;
+
 int n;
-struct node
+int ret[N];//储存最终结果
+struct node //同时用于储存牛和牛棚数据
 {
-// 存每⼀头⽜的信息：开始时间，结束时间，排序之前的位置，分配的⽜棚号
-int x, y, pos, num;
-}a[N];
-int cnt;
-int ret[N];
-multimap<int, int> st;
-bool cmp(node& x, node& y)
+    int x; // 开始时间 / 结束时间
+    int y; // 结束时间 / 牛棚编号
+    int i; //排序前编号
+
+bool operator<(const node& a) const //默认是大堆，仿函数使用less
 {
-return x.x < y.x;
+return x>a.x;
 }
+
+}a[N];
+
+bool cmp(node& a,node& b)
+{
+    return a.x<b.x;
+}
+
 int main()
 {
-cin >> n;
-for(int i = 1; i <= n; i++)
+    cin >> n;
+    for(int i = 1; i <= n; i++) 
+    {
+        cin >> a[i].x >> a[i].y;
+        a[i].i = i;
+    }
+
+sort(a+1,a+1+n,cmp);
+priority_queue<node> heap;//用小根堆存放牛棚
+int cnt=1;ret[a[1].i]=1;
+
+heap.push({a[1].y,1});
+
+for(int i=2;i<=n;i++)
 {
-cin >> a[i].x >> a[i].y;
-a[i].pos = i;
-}
-sort(a + 1, a + 1 + n, cmp);
-for(int i = 1; i <= n; i++)
+int l=a[i].x,r=a[i].y;node t=heap.top();
+if(l>t.x)
 {
-int l = a[i].x, r = a[i].y;
-auto it = st.upper_bound(l);
-// 不能接在任何⼀个区间后⾯，那就新开⼀个区间
-if(it == st.end())
+    heap.pop();ret[a[i].i]=t.y;
+    heap.push({r,t.y});
+}
+else ret[a[i].i]=++cnt,heap.push({r,cnt});
+}
+
+cout<<cnt<<endl;
+for(int i=1;i<=n;i++)
 {
-st.insert({r, ++cnt});
-a[i].num = cnt;
+    cout<<ret[i]<<endl;
 }
-else // 能接在某个区间后⾯，那就接上去
-{
-int p = it->second;
-st.erase(it); // 把之前⽜删掉
-st.insert({r, p}); // 新来的⽜放进去
-a[i].num = p;
-}
-}
-cout << cnt << endl;
-for(int i = 1; i <= n; i++)
-{
-ret[a[i].pos] = a[i].num;
-}
-for(int i = 1; i <= n; i++) cout << ret[i] << endl;
+
 return 0;
 }
-
-
-
